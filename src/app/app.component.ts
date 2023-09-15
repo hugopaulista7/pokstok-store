0d4ddcb7ae995c60a6e13b5b0f93d486998e913a
Hugo Paulista <hugo.paulista@foton.la>
2023-09-15 15:48:16 -0300

    integrando com firebase e analytics funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import * as firebaseAnalytics from 'firebase/analytics';
+import { environment } from 'src/environments/environment.dev';
+
+export const app = initializeApp(environment.firebaseConfig);
+export const analytics = firebaseAnalytics.getAnalytics(app);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'poktstok';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    firebaseAnalytics.logEvent(analytics, 'Initialiized App');
+  }
 }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBW6Rkt8YmJ-FjT3kxPJplBnfdIpOkcCyI',
+    authDomain: 'pokstok-store.firebaseapp.com',
+    projectId: 'pokstok-store',
+    storageBucket: 'pokstok-store.appspot.com',
+    messagingSenderId: '1046135982154',
+    appId: '1:1046135982154:web:68f0ed878270220b1b3eb3',
+    measurementId: 'G-EE5D44BKVF',
+  },
+};
