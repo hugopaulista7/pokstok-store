dde7cae9ca6fc6d21355533a0ffbcdf9b365e28a
Hugo Paulista <hugo.paulista@foton.la>
2023-09-19 15:43:52 -0300

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/home',
     pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
   },
   {
     path: 'login',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components.module';
+import { CoolSocialLoginButtonsModule } from '@angular-cool/social-login-buttons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { LoginPageComponent } from './login-page/login-page.component';
 
+import { IconAppComponent } from './icon-app/icon-app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    IconAppComponent,
+    HomePageComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    ButtonModule,
+    ProgressSpinnerModule,
+    CardModule,
+    CoolSocialLoginButtonsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
